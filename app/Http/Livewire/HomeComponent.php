4aaf93649bf7a53b9546db879b08d16c307728ff
mahmoud <malmalah04@gmail.com>
2022-12-06 00:10:06 +0200

    2

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -23,9 +23,9 @@ class HomeComponent extends Component
         $sliders = HomeSlider::where('status',1)->get();
         $lproducts = Product::orderBy('created_at','DESC')->get()->take(8);
         $category = HomeCategory::find(1);
-        $cats = explode(',',$category->sel_categories);
+        $cats = explode(',',$category->sel_categories ?? Null);
         $categories = Category::whereIn('id',$cats)->get();
-        $no_of_products = $category->no_of_products;
+        $no_of_products = $category->no_of_products ?? Null;
         $sale = Sale::find(1);
         $sproducts = Product::where('sale_price','>',0)->inRandomOrder()->get()->take(8);
         return view('livewire.home-component',['sliders'=>$sliders,'lproducts'=>$lproducts,'categories'=>$categories,'no_of_products'=>$no_of_products,'sproducts'=>$sproducts,'sale'=>$sale])->layout('layouts.base');
